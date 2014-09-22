110a583ad36f5fc70bee6ae92d9d60d0f677cb13
HarshHarwani <harwaniharsh@gmail.com>
2014-09-22 02:46:39 -0400

    Removed normalizer code from accent.Implemented using HashMaps.

--- IR_project1/src/edu/buffalo/cse/irf14/analysis/AccentFilter.java
+++ IR_project1/src/edu/buffalo/cse/irf14/analysis/AccentFilter.java
@@ -1,6 +1,5 @@
 package edu.buffalo.cse.irf14.analysis;
-
-import java.text.Normalizer;
+import java.util.HashMap;
 /**
  * @author Harsh
  * @parameter {@link TokenStream}
@@ -11,6 +10,7 @@ public class AccentFilter extends TokenFilter {
 
 
 	TokenStream tStream=null;
+	boolean accentFlag=false;
 	public AccentFilter(TokenStream stream) {
 		super(stream);
 		this.tStream=stream;
@@ -19,17 +19,129 @@ public class AccentFilter extends TokenFilter {
 
 
 	TokenStream tokenStream=new TokenStream();
+	private static final HashMap<String,String> accentMap =  new HashMap<String,String>() ;
+	static{
+		accentMap.put("À", "A");
+		accentMap.put("Á", "A");
+		accentMap.put("Â", "A");
+		accentMap.put("Ã", "A");
+		accentMap.put("Ä", "A");
+		accentMap.put("Å", "A");
+		accentMap.put("Æ", "AE");
+		accentMap.put("Ç", "C");
+		accentMap.put("È", "E");
+		accentMap.put("É", "E");
+		accentMap.put("Ê", "E");
+		accentMap.put("Ë", "E");
+		accentMap.put("Ì", "I");
+		accentMap.put("Í", "I");
+		accentMap.put("Î", "I");
+		accentMap.put("Ï", "I");
+		accentMap.put("Ĳ", "IJ");
+		accentMap.put("Ð", "D");
+		accentMap.put("Ñ", "N");
+		accentMap.put("Ò", "O");
+		accentMap.put("Ó", "O");
+		accentMap.put("Ô", "O");
+		accentMap.put("Õ", "O");
+		accentMap.put("Ö", "O");
+		accentMap.put("Ø", "O");
+		accentMap.put("Œ", "OE");
+		accentMap.put("Þ","TH");
+		accentMap.put("Ù", "U");
+		accentMap.put("Ú", "U");
+		accentMap.put("Û", "U");
+		accentMap.put("Ü", "U");
+		accentMap.put("Ý", "Y");
+		accentMap.put("Ÿ", "Y");
+		accentMap.put("à", "a");
+		accentMap.put("á", "a");
+		accentMap.put("â", "a");
+		accentMap.put("ã", "a");
+		accentMap.put("ä", "a");
+		accentMap.put("å", "a");
+		accentMap.put("æ", "ae");
+		accentMap.put("ç", "c");
+		accentMap.put("è", "e");
+		accentMap.put("é", "e");
+		accentMap.put("ê", "e");
+		accentMap.put("ë", "e");
+		accentMap.put("ì", "i");
+		accentMap.put("í", "i");
+		accentMap.put("î", "i");
+		accentMap.put("ï", "i");
+		accentMap.put("ĳ", "ij");
+		accentMap.put("ð", "d");
+		accentMap.put("ñ", "n");
+		accentMap.put("ò", "o");
+		accentMap.put("ó", "o");
+		accentMap.put("ô", "o");
+		accentMap.put("õ", "o");
+		accentMap.put("ö", "o");
+		accentMap.put("ø", "o");
+		accentMap.put("œ", "oe");
+		accentMap.put("ß", "ss");
+		accentMap.put("þ", "th");
+		accentMap.put("ù", "u");
+		accentMap.put("ú", "u");
+		accentMap.put("û", "u");
+		accentMap.put("ü", "u");
+		accentMap.put("ý", "y");
+		accentMap.put("ÿ", "y");
+		accentMap.put("ﬀ", "ff");
+		accentMap.put("ﬁ", "fi");
+		accentMap.put("ﬂ", "fl");
+		accentMap.put("ﬃ", "ffi");
+		accentMap.put("ﬄ", "ffl");
+		accentMap.put("ﬅ", "ft");
+		accentMap.put("ﬆ", "st");
+
+	}
 	public TokenStream accentFilter(TokenStream tStream)
 	{
-
+		String finalString=null;
+		String filterString=null;
+		String mapKey=null;
+		String value=null;
+		accentFlag=false;
 		while(tStream.hasNext())
 		{
 			tStream.next();
+
+
+			int count=0;
 			Token tokens=tStream.getCurrent();
 			String token=tokens.getTermText();
-			String filteredToken = Normalizer.normalize(token, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+			String[] filter=token.split("");
+
+			for(String s:filter)
+			{
+				if(accentMap.containsKey(s))
+				{
+					for(String key: accentMap.keySet())
+					{
+						value=accentMap.get(s);
+						if(key.equals(s))
+						{
+							mapKey=key;
+							accentFlag=true;
+							count++;
+							break;
+
+						}
+					}
+					filterString=mapKey;
+					if(count<2)
+						finalString=token.replace(filterString,value);
+					if(count>=2)
+						finalString=finalString.replace(filterString,value);
+				}
+			}
 			Token token2 = new Token();
-			token2.setTermText(filteredToken);
+			if(accentFlag)
+				token2.setTermText(finalString);
+			else
+				token2.setTermText(token);
 			tokenStream.addTokenToStream(token2);
 		}
 		return tokenStream;		
